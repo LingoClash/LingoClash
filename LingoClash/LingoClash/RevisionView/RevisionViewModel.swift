d906e566ee5b05a5569b8bdd01165fe009beb506
kevin chua <skullspine9@gmail.com>
2022-04-02 11:57:08 +0800

    Add revision view models

---
+++ LingoClash/LingoClash/RevisionView/RevisionViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  RevisionViewModel.swift
+//  LingoClash
+//
+//  Created by kevin chua on 2/4/22.
+//
+
+import Combine
+
+final class RevisionViewModel {
+    
+//    @Published var currentBookProgress: BookProgress?
+//    @Published var currentBook: Book?
+//    @Published var lessonSelectionViewModel: LessonSelectionViewModel?
+    
+    func fetchDecks() {
+        //TODO: get current decks from db
+    //        self.currentBookProgress = BookProgress(name: "Chinese 1", progress: "0/10")
+    //        self.currentBook = Book(id: "1", category_id: "1", name: "Chinese 1")
+    //        guard let currentBook = currentBook else {
+    //            return
+    //        }
+    //        self.lessonSelectionViewModel = LessonSelectionViewModelFromBook(book: currentBook)
+        
+    }
+    
+    func addDeck(_ deckFields: CreateDeckFields) {
+
+    }
+}
