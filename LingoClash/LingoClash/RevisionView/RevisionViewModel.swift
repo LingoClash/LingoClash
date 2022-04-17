a359645f1047c8d0e690b00d15b661eccb625532
kevin chua <skullspine9@gmail.com>
2022-04-17 20:11:30 +0800

    Remove default decks

--- LingoClash/LingoClash/RevisionView/RevisionViewModel.swift
+++ LingoClash/LingoClash/RevisionView/RevisionViewModel.swift
@@ -18,43 +18,6 @@ final class RevisionViewModel {
         return revisionQuery.magnitude < 2
     }
     
-    private static let DEFAULT_DECK_NAME = "Default Deck"
-    
-    func addDefaultDeck() {
-        decks.append(
-            Deck(
-                name: RevisionViewModel.DEFAULT_DECK_NAME,
-                vocabs: createRevisionVocabs()
-            )
-        )
-    }
-    
-    func createRevisionVocabs() -> [RevisionVocab] {
-        let vocabs = [
-            Vocab(vocabId: 1, word: "周", definition: "week", sentence: "一周有七天。",
-                  sentenceDefinition: "Every week has 7 days.", pronunciationText: "zhōu"),
-            Vocab(vocabId: 2, word: "今天", definition: "today", sentence: "她今天看起来很悲伤。",
-                  sentenceDefinition: "She looks saf today.", pronunciationText: "jīntiān"),
-            Vocab(vocabId: 3, word: "明天", definition: "tomorrow", sentence: "明天10：10",
-                  sentenceDefinition: "tomorrow at 10:10", pronunciationText: "míngtiān"),
-            Vocab(vocabId: 4, word: "昨天", definition: "yesterday", sentence: "她昨天看起来很悲伤。",
-                  sentenceDefinition: "She looks saf today.", pronunciationText: "jīntiān"),
-            Vocab(vocabId: 5, word: "日历", definition: "calendar", sentence: "她今天看起来很日历。",
-                  sentenceDefinition: "She looks saf today.", pronunciationText: "jīntiān"),
-            Vocab(vocabId: 6, word: "秒", definition: "second", sentence: "她秒看起来很悲伤。",
-                  sentenceDefinition: "She looks saf today.", pronunciationText: "jīntiān")
-        ]
-        var vocabArr: [RevisionVocab] = []
-        
-        for vocab in vocabs {
-            vocabArr.append(
-                RevisionVocab(id: "-1", vocab: vocab, difficultyParameter: Difficulty(amount: 0))
-            )
-        }
-        
-        return vocabArr
-    }
-
     func fetchDecks() {
         self.decks = []
         self.isRefreshing = true
@@ -69,8 +32,6 @@ final class RevisionViewModel {
         }.catch { error in
             Logger.error("\(error)")
         }
-
-        addDefaultDeck()
     }
 
     
